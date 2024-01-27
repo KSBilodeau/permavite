ce19f12238c91e7bb63a182e56b913070025087c
KSBilodeau <22333708+KSBilodeau@users.noreply.github.com>
2024-01-26 23:38:18 -0600

    Create basic web server

--- backend/permaserver/src/main.rs
+++ backend/permaserver/src/main.rs
@@ -1,3 +1,9 @@
+use std::net::TcpListener;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for Ok(_) in listener.incoming() {
+        println!("Connection established!")
+    }
 }
