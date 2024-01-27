e303547ef97c05fa386e8864901197342016036a
KSBilodeau <22333708+KSBilodeau@users.noreply.github.com>
2024-01-27 09:51:28 -0600

    Add api plugin registration route

--- backend/permaserver/src/main.rs
+++ backend/permaserver/src/main.rs
@@ -1,9 +1,24 @@
-use rouille::Response;
+use rouille::{Response, router};
 
 fn main() {
     rouille::start_server("127.0.0.1:7878", move |request| {
-        println!("{}", request.raw_query_string());
+        println!("{}", request.url());
+        let result = router!(request,
+            (GET) (/) => {
+                "MAIN"
+            },
+            (GET) (/api/) => {
+                "LATEST PERMAVITE API: PAPI V1"
+            },
+            (GET) (/invite/) => {
+                "INVITE"
+            },
+            (POST) (/api/v1/register_plugin) => {
+                "REGISTER"
+            },
+            _ => "UNKNOWN"
+        );
 
-        Response::text("Hello, World!")
+        Response::text(result)
     })
 }
