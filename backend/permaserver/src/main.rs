818c9b3fc990cb723cc16654a367836b010bd9c2
KSBilodeau <22333708+KSBilodeau@users.noreply.github.com>
2024-01-27 20:26:53 -0600

    Fix server implementation

--- backend/permaserver/src/main.rs
+++ backend/permaserver/src/main.rs
@@ -31,7 +31,6 @@ fn main() -> anyhow::Result<()> {
     // Create all the database objects to prep for future transactions
 
     let _userdb = Database::create(&*shellexpand::tilde("~/userdb"))?;
-    let linkdb = Database::create(&*shellexpand::tilde("~/linkdb"))?;
     let _accessdb = Database::create(&*shellexpand::tilde("~/accessdb"))?;
     let _plugindb = Database::create(&*shellexpand::tilde("~/plugindb"))?;
 
@@ -40,36 +39,68 @@ fn main() -> anyhow::Result<()> {
     rouille::start_server("127.0.0.1:7878", move |request| {
         router!(request,
             (GET) (/invite/{id: String}) => {
+                // Read the database from its file
+                let linkdb = Database::create(&*shellexpand::tilde("~/linkdb")).unwrap();
                 // Prepare read transaction on the link database
                 let read = linkdb.begin_read().unwrap();
                 // Open the link table for reading using the LINK_TABLE format
                 let table = read.open_table(LINK_TABLE).unwrap();
 
-                if let Some(link) = table.get(id.as_str()).unwrap() {
-                    let (link_code, channel_id) = link.value();
+                let resp = {
+                    if let Some(link) = table.get(id.as_str()).unwrap() {
+                        let link_code = String::from(link.value().0);
+                        let channel_id = link.value().1;
 
-                    let link_state = ureq::get(format!("https://discord.com/api/v10/invites/{}", link_code).as_str())
-                        .call()
-                        .unwrap();
-
-                    if link_state.status() == 404 {
-                        let new_link = ureq::post(format!("https://discord.com/api/v10/channels/{}/invites", channel_id).as_str())
-                            .set("Authorization", env!("BOT_TOKEN"))
-                            .send_json(
-                                ureq::json!({
-                                    "max_age": 0,
-                                    "target_type": 2,
-                                    "target_application_id": 1200665926098497576,
-                                }))
+                        let link_state = ureq::get(format!("https://discord.com/api/v10/invites/{}", link_code).as_str())
+                            .call()
                             .unwrap();
 
-                        todo!()
+                        if link_state.status() == 404 {
+                            let new_link = ureq::post(format!("https://discord.com/api/v10/channels/{}/invites", channel_id).as_str())
+                                .set("Authorization", env!("BOT_TOKEN"))
+                                .send_json(
+                                    ureq::json!({
+                                        "max_age": 0,
+                                        "target_type": 2,
+                                        "target_application_id": 1200665926098497576u64,
+                                    }))
+                                .unwrap();
+
+                            let resp_json: Vec<String> = new_link.into_string()
+                                .unwrap()
+                                .split('\"')
+                                .map(String::from)
+                                .collect();
+
+                            Response::text(format!("https://discord.gg/{}", resp_json[4]))
+                        } else {
+                            Response::redirect_303(String::from(link.value().0))
+                        }
+                    } else {
+                        Response::redirect_303("https://permavite.com/404.html")
                     }
+                };
 
-                    Response::redirect_303(link.value())
-                }
+                resp
+            },
+            (POST) (/api/v1/generate_perma_link/{channel_id: u64}) => {
+                let new_link = ureq::post(format!("https://discord.com/api/v10/channels/{}/invites", channel_id).as_str())
+                    .set("Authorization", env!("BOT_TOKEN"))
+                    .send_json(
+                        ureq::json!({
+                            "max_age": 0,
+                            "target_type": 2,
+                            "target_application_id": 1200665926098497576u64,
+                        }))
+                    .unwrap();
 
-                Response::redirect_303("https://permavite.com/404.html")
+                let resp_json: Vec<String> = new_link.into_string()
+                                .unwrap()
+                                .split('\"')
+                                .map(String::from)
+                                .collect();
+
+                Response::text(format!("https://discord.gg/{}", resp_json[4]))
             },
             _ => Response::text("Unknown")
         )
