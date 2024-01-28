4f222b4edd3026b8d11c34e9b796b8beb31fc79b
KSBilodeau <22333708+KSBilodeau@users.noreply.github.com>
2024-01-27 20:07:13 -0600

    Restart making the whole server

--- backend/permaserver/src/main.rs
+++ backend/permaserver/src/main.rs
@@ -1,55 +1,77 @@
-use redb::{Database, Error, ReadableTable, TableDefinition};
-use rouille::{Response, router};
+use redb::{Database, ReadableTable, TableDefinition};
+use rouille::{router, Response};
+use std::fs::File;
+use std::path::Path;
 
 const _USER_TABLE: TableDefinition<&str, Vec<&str>> = TableDefinition::new("user_data");
-const LINK_TABLE: TableDefinition<&str, &str> = TableDefinition::new("link_data");
+const LINK_TABLE: TableDefinition<&str, (&str, u64)> = TableDefinition::new("link_data");
+const _ACCESS_TABLE: TableDefinition<&str, i64> = TableDefinition::new("access_data");
+const _PLUGIN_TABLE: TableDefinition<&str, (&str, &str, &str, Vec<u8>)> =
+    TableDefinition::new("plugin_data");
 
-#[cfg(not(target_os = "wasi"))]
-fn main() -> Result<(), Error>{
-    // let users = Database::create("/Users/ksbilodeau/userdb")?;
-    // let write = users.begin_write()?;
-    // {
-    //     let mut table = write.open_table(USER_TABLE)?;
-    //     table.insert("ksbilodeau", &vec!["gay", "meep"])?;
-    //     table.insert("moosh", &vec!["demonic"])?;
-    // }
-    // write.commit()?;
-    //
-    // let links = Database::create("/Users/ksbilodeau/linkdb")?;
-    // let write = links.begin_write()?;
-    // {
-    //     let mut table = write.open_table(LINK_TABLE)?;
-    //     table.insert("gay", "https://discord.gg/H7mpzzXE")?;
-    //     table.insert("meep", "https://discord.gg/uyWEmbY")?;
-    //     table.insert("demonic", "https://discord.gg/tNDTnJd")?;
-    // }
-    // write.commit()?;
+fn main() -> anyhow::Result<()> {
+    // Create all the database files if they don't exist yet
 
-    let _ = Database::create("~/userdb")?;
-    let linkdb = Database::create("~/linkdb")?;
+    if Path::new(&*shellexpand::tilde("~/userdb")).exists() {
+        File::create(&*shellexpand::tilde("~/userdb"))?;
+    }
+
+    if Path::new(&*shellexpand::tilde("~/linkdb")).exists() {
+        File::create(&*shellexpand::tilde("~/linkdb"))?;
+    }
+
+    if Path::new(&*shellexpand::tilde("~/accessdb")).exists() {
+        File::create(&*shellexpand::tilde("~/accessdb"))?;
+    }
+
+    if Path::new(&*shellexpand::tilde("~/plugindb")).exists() {
+        File::create(&*shellexpand::tilde("~/plugindb"))?;
+    }
+
+    // Create all the database objects to prep for future transactions
+
+    let _userdb = Database::create(&*shellexpand::tilde("~/userdb"))?;
+    let linkdb = Database::create(&*shellexpand::tilde("~/linkdb"))?;
+    let _accessdb = Database::create(&*shellexpand::tilde("~/accessdb"))?;
+    let _plugindb = Database::create(&*shellexpand::tilde("~/plugindb"))?;
+
+    // Start the server and start routing non-nginx managed pathways
 
     rouille::start_server("127.0.0.1:7878", move |request| {
-        println!("{}", request.url());
-        let result = router!(request,
-            (GET) (/api/) => {
-                "LATEST PERMAVITE API: PAPI V1".into()
-            },
-            (GET) (/invite/{id: String }) => {
+        router!(request,
+            (GET) (/invite/{id: String}) => {
+                // Prepare read transaction on the link database
                 let read = linkdb.begin_read().unwrap();
+                // Open the link table for reading using the LINK_TABLE format
                 let table = read.open_table(LINK_TABLE).unwrap();
 
-                let Some(link) = table.get(id.as_str()).unwrap() else {
-                   return Response::empty_404();
-                };
+                if let Some(link) = table.get(id.as_str()).unwrap() {
+                    let (link_code, channel_id) = link.value();
 
-                format!("INVITE LINK: {}", link.unwrap().value())
-            },
-            (POST) (/api/v1/register_plugin) => {
-                "REGISTER PLUGIN".into()
-            },
-            _ => "UNKNOWN".into()
-        );
+                    let link_state = ureq::get(format!("https://discord.com/api/v10/invites/{}", link_code).as_str())
+                        .call()
+                        .unwrap();
 
-        Response::text(result)
-    })
+                    if link_state.status() == 404 {
+                        let new_link = ureq::post(format!("https://discord.com/api/v10/channels/{}/invites", channel_id).as_str())
+                            .set("Authorization", env!("BOT_TOKEN"))
+                            .send_json(
+                                ureq::json!({
+                                    "max_age": 0,
+                                    "target_type": 2,
+                                    "target_application_id": 1200665926098497576,
+                                }))
+                            .unwrap();
+
+                        todo!()
+                    }
+
+                    Response::redirect_303(link.value())
+                }
+
+                Response::redirect_303("https://permavite.com/404.html")
+            },
+            _ => Response::text("Unknown")
+        )
+    });
 }
